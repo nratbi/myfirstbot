1162e9c7c69c49723b74eb18d3e8e518598cc037
Nabil <nabil@sparkup.fr>
2017-09-24 00:14:08 +0200

    make it better

--- API_bot.py
+++ API_bot.py
@@ -8,31 +8,6 @@ from flask import got_request_exception
 import os
 
 app = Flask(__name__) 
-@app.before_first_request
-def init_rollbar():
-    """init rollbar module"""
-    rollbar.init(
-        # access token for the demo app: https://rollbar.com/demo
-        '0200a206c3934cc99fabbdad7ad20da3',
-        # environment name
-        '.env',
-        # server root directory, makes tracebacks prettier
-        root=os.path.dirname(os.path.realpath(__file__)),
-        # flask already sets up logging
-        allow_logging_basic_config=False)
-
-    # send exceptions from `app` to rollbar, using flask's signal system.
-    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
-
-class CustomRequest(Request):
-    @property
-    def rollbar_person(self):
-        # 'id' is required, 'username' and 'email' are indexed but optional.
-        # all values are strings.
-        return {'id': '123', 'username': 'admin', 'email': 'nabil.ratbienstb@gmail.com'}
-
-app.request_class = CustomRequest
-
 @app.route("/response/", methods=['GET','POST'])
 def response():
 	m = request.get_json()
