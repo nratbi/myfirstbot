81a8bc4786c6d9c3c5db682adf579785bbdfb7e9
Nabil <nabil@sparkup.fr>
2017-09-24 16:25:03 +0200

    update

--- API_bot.py
+++ API_bot.py
@@ -10,7 +10,7 @@ from flask import Response
 app = Flask(__name__) 
 @app.route("/", methods=['GET','POST'])
 def facebook_verification():
-	print(request.data)
+	print(request.args)
 	return Response("ok", status=200, mimetype='application/json')
 
 @app.route("/response/", methods=['GET','POST'])
