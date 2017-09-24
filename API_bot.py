48de64ce1fc77098efcecee1d5872f0da5bca0ed
Nabil <nabil@sparkup.fr>
2017-09-24 21:09:56 +0200

    update

--- API_bot.py
+++ API_bot.py
@@ -69,7 +69,7 @@ def webhook():
                     })                   
                     r = requests.post('https://api.api.ai/v1/query?v=20150910',headers=headers, data=content)
                     if r:
-                        r2 = requests.get('https://myfirstbot11.herokuapp.com/response/', json = r.json())
+                        r2 = requests.post('https://myfirstbot11.herokuapp.com/response/', json = r.json())
                     send_message(sender_id, "roger that!")
 
     return "ok", 200
