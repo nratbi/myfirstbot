2fddc4eb4e17d317218fed96ae087b50d8b8bbfb
Nabil <nabil@sparkup.fr>
2017-09-24 20:28:00 +0200

    update

--- API_bot.py
+++ API_bot.py
@@ -10,71 +10,71 @@ import sys
 import json
 import requests
 
-def send_message(recipient_id, message_text):
+# def send_message(recipient_id, message_text):
 
-    params = {
-        "access_token": "EAAXFoXg4V2oBANtEWids8btXLLN3xMfu2xkZBoaQqwmkSZCheKJZCbZABG8Cmb1hamD0ZCZAK5DZCLYQmU2eXnsGB6pAJ3TZATvFjGczUeCwEsJvFXJujrw7DtF9CZBwPk9tixFUJ134Fj5HrgMtBOlw8KnHsst46IjTB5kv4XolZB9QZDZD"
-    }
-    headers = {
-        "Content-Type": "application/json"
-    }
-    data = json.dumps({
-        "recipient": {
-            "id": recipient_id
-        },
-        "message": {
-            "text": message_text
-        }
-    })
-    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+#     params = {
+#         "access_token": "EAAXFoXg4V2oBANtEWids8btXLLN3xMfu2xkZBoaQqwmkSZCheKJZCbZABG8Cmb1hamD0ZCZAK5DZCLYQmU2eXnsGB6pAJ3TZATvFjGczUeCwEsJvFXJujrw7DtF9CZBwPk9tixFUJ134Fj5HrgMtBOlw8KnHsst46IjTB5kv4XolZB9QZDZD"
+#     }
+#     headers = {
+#         "Content-Type": "application/json"
+#     }
+#     data = json.dumps({
+#         "recipient": {
+#             "id": recipient_id
+#         },
+#         "message": {
+#             "text": message_text
+#         }
+#     })
+#     r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
 
 
-app = Flask(__name__) 
-@app.route("/", methods=['GET'])
-def verify():
-    # when the endpoint is registered as a webhook, it must echo back
-    # the 'hub.challenge' value it receives in the query arguments
-    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
-        if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
-            return "Verification token mismatch", 403
-        return request.args["hub.challenge"], 200
+# app = Flask(__name__) 
+# @app.route("/", methods=['GET'])
+# def verify():
+#     # when the endpoint is registered as a webhook, it must echo back
+#     # the 'hub.challenge' value it receives in the query arguments
+#     if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+#         if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
+#             return "Verification token mismatch", 403
+#         return request.args["hub.challenge"], 200
 
-    return "ok", 200
+#     return "ok", 200
 
-@app.route('/', methods=['POST'])
-def webhook():
-    # endpoint for processing incoming messaging events
-    data = request.get_json()
+# @app.route('/', methods=['POST'])
+# def webhook():
+#     # endpoint for processing incoming messaging events
+#     data = request.get_json()
 
-    if data["object"] == "page":
+#     if data["object"] == "page":
 
-        for entry in data["entry"]:
-            for messaging_event in entry["messaging"]:
+#         for entry in data["entry"]:
+#             for messaging_event in entry["messaging"]:
 
-                if messaging_event.get("message"):  # someone sent us a message
+#                 if messaging_event.get("message"):  # someone sent us a message
 
-                    sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
-                    recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-                    message_text = messaging_event["message"]["text"]  # the message's text
-                    params = {
-                    "access_token": "EAAXFoXg4V2oBANtEWids8btXLLN3xMfu2xkZBoaQqwmkSZCheKJZCbZABG8Cmb1hamD0ZCZAK5DZCLYQmU2eXnsGB6pAJ3TZATvFjGczUeCwEsJvFXJujrw7DtF9CZBwPk9tixFUJ134Fj5HrgMtBOlw8KnHsst46IjTB5kv4XolZB9QZDZD"
-                    } 
-                    headers = {
-                    "Content-Type": "application/json"
-                    }
-                    content = json.dumps({
-                    "query": message_text,
-                    "timezone": "Europe/Paris",
-                    "lang": "fr",
-                    "contexts" : [],
-                    "sessionId": "c2f3eb24-8ed5-42c3-9ec1-ee51f0bb607c"
-                    })                   
-                    r = requests.post('https://api.api.ai/v1/query?v=20150910', params=params, headers=headers, data=content)
-                    if r:
-                        print(r.json())
-                    send_message(sender_id, "roger that!")
+#                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
+#                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
+#                     message_text = messaging_event["message"]["text"]  # the message's text
+#                     params = {
+#                     "access_token": "EAAXFoXg4V2oBANtEWids8btXLLN3xMfu2xkZBoaQqwmkSZCheKJZCbZABG8Cmb1hamD0ZCZAK5DZCLYQmU2eXnsGB6pAJ3TZATvFjGczUeCwEsJvFXJujrw7DtF9CZBwPk9tixFUJ134Fj5HrgMtBOlw8KnHsst46IjTB5kv4XolZB9QZDZD"
+#                     } 
+#                     headers = {
+#                     "Content-Type": "application/json"
+#                     }
+#                     content = json.dumps({
+#                     "query": message_text,
+#                     "timezone": "Europe/Paris",
+#                     "lang": "fr",
+#                     "contexts" : [],
+#                     "sessionId": "c2f3eb24-8ed5-42c3-9ec1-ee51f0bb607c"
+#                     })                   
+#                     r = requests.post('https://api.api.ai/v1/query?v=20150910', params=params, headers=headers, data=content)
+#                     if r:
+#                         print(r.json())
+#                     send_message(sender_id, "roger that!")
 
-    return "ok", 200
+#     return "ok", 200
 
 
 @app.route('/response/', methods=['GET,POST'])
