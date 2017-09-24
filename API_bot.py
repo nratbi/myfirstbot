4b6f0c2b9638170b9deb836a32204edcaccbc6a5
Nabil <nabil@sparkup.fr>
2017-09-24 12:23:44 +0200

    update

--- API_bot.py
+++ API_bot.py
@@ -3,14 +3,19 @@
 from flask import jsonify, request, Flask
 from flask import Request
 import os
+from pymongo import MongoClient
 
 app = Flask(__name__) 
 @app.route("/response/", methods=['GET','POST'])
 def response():
+	client = MongoClient(host=['localhost:27017'])
+	computers = client["admin"].computers
 	m = request.get_json()
 	indicators = m['result']['parameters']
 	if 'game' in indicators:
-		print('Le user est un gamer')
+		find_pc_gamer = computers.aggregate([{'$project':{'total':{'$sum' : ['processeur_rate','carte_graphique_rate']}}}])
+		for item in find_pc_gamer:
+			print(item)
 	return jsonify(m)
 
 
