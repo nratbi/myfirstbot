d0b91595d9be6b8fec5d39d0b6639fca05a458c0
Nabil <nabil@sparkup.fr>
2017-09-24 18:47:59 +0200

    update

--- API_bot.py
+++ API_bot.py
@@ -25,6 +25,8 @@ def send_message(recipient_id, message_text):
             "text": message_text
         }
     })
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+
 
 app = Flask(__name__) 
 @app.route("/", methods=['GET'])
@@ -61,7 +63,6 @@ def webhook():
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
-                    print(message_text)
                     send_message(sender_id, "roger that!")
 
                 if messaging_event.get("delivery"):  # delivery confirmation
