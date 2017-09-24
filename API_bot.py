089bd6d983e117d5f8d857fda08a602035005f08
Nabil <nabil@sparkup.fr>
2017-09-24 13:31:31 +0200

    update

--- API_bot.py
+++ API_bot.py
@@ -14,7 +14,7 @@ def response():
 	indicators = m['result']['parameters']
 	if 'game' in indicators:
 		# find_pc_gamer = computers.aggregate([{'$project':{'gamer_rate':{'$add' :['$processeur_rate','$carte_graphique_rate']}}}])
-		find_pc_gamer = computers.update({},{'$set':{'gamer_rate':{'$add' :['$processeur_rate','$carte_graphique_rate']}}},multi=True)
+		find_pc_gamer = computers.aggregate({"$addFields":{'gamer_rate':{'$add' :['$processeur_rate','$carte_graphique_rate']}}},{'$out':'computers'})
 		for item in find_pc_gamer:
 			print(item)
 	return jsonify(m)
