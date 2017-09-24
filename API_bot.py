368a7401ad5ea7efc02e7118a6dd9fbeab803317
Nabil <nabil@sparkup.fr>
2017-09-24 22:53:25 +0200

    update

--- API_bot.py
+++ API_bot.py
@@ -69,14 +69,14 @@ def webhook():
                     })                   
                     r = requests.post('https://api.api.ai/v1/query?v=20150910',headers=headers, data=content)
                     if r:
-                        r2 = requests.post('https://myfirstbot11.herokuapp.com/response', json = r.json())
+                        r2 = requests.post('https://myfirstbot11.herokuapp.com/response/', json = {'test':'voila'})
                         print(r2.content)
                     send_message(sender_id, "roger that!")
 
     return "ok", 200
 
 
-@app.route('/response', methods=['GET,POST'])
+@app.route('/response/', methods=['GET,POST'])
 def response():
     client = MongoClient("mongodb://heroku_fkfhqw1w:mtkhac4bj08bu2qs02gm0i4s79@ds147964.mlab.com:47964/heroku_fkfhqw1w")
     computers = client["heroku_fkfhqw1w"].computers
