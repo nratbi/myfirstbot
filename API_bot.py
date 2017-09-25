40cfae1d65b83e062a788cfc760437d3a34c67e3
Nabil <nabil@sparkup.fr>
2017-09-25 21:08:56 +0200

    update

--- API_bot.py
+++ API_bot.py
@@ -15,8 +15,10 @@ import numpy as np
 import numbers
 
 def calculate_utility(weights, vector, maxs_criteria, mins_criteria):
-    print(vector)
-    utility_vector = [(weights[i]/sum(weights))*(np.nansum([vector[i]])-mins_criteria[i])/(maxs_criteria[i]-mins_criteria[i]) for i in range(len(vector))]
+    for i in range(len(vector)):
+        if vector[i] == float('nan'):
+            vector[i] = 0
+    utility_vector = [(weights[i]/sum(weights))*(vector[i]-mins_criteria[i])/(maxs_criteria[i]-mins_criteria[i]) for i in range(len(vector))]
     utility = sum(utility_vector)
     print(utility_vector)
     return utility
