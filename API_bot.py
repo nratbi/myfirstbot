49f30654f2c93a34d75311311a7afb0e0e0c72fc
Nabil <nabil@sparkup.fr>
2017-09-25 18:53:58 +0200

    update

--- API_bot.py
+++ API_bot.py
@@ -114,7 +114,7 @@ def response():
 
             if 'game' in indicators and indicators['game'] != '':
                 if 'pc_fixe' in indicators and indicators['pc_fixe'] != '':
-                    find_pc_gamer = list(computers.find({'type':'fixe'}))
+                    find_pc_gamer = pd.DataFrame(list(computers.find({'type':'fixe'})))
                     speech = 'MacBook for sure!'
                     print(find_pc_gamer)
                 elif 'type' in indicators and indicators['type'] != '':
