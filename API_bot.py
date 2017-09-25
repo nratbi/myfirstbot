d075d2ca58950cf6eed1c2af7374031993c7d07b
Nabil <nabil@sparkup.fr>
2017-09-25 21:00:14 +0200

    update

--- API_bot.py
+++ API_bot.py
@@ -15,8 +15,10 @@ import numpy as np
 import numbers
 
 def calculate_utility(weights, vector, maxs_criteria, mins_criteria):
+    print(vector)
     utility_vector = [(weights[i]/sum(weights))*(vector[i]-mins_criteria[i])/(maxs_criteria[i]-mins_criteria[i]) for i in range(len(vector))]
     utility = sum(utility_vector)
+    print(utility_vector)
     return utility
 
 def send_message(recipient_id, message_text):
